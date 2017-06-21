b85c1a0ccf196c2d8442f473b812cf8096fd4b4c
user <user@IBM197-R90G945T>
2017-06-21 17:42:13 +0200

    deleting stuff

--- SalesSale/src/Main.java
+++ SalesSale/src/Main.java
@@ -1,14 +1,8 @@
-
+ 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello");
-		
-		System.out.println("Hello 3");
-		System.out.println("Hello 4");
-		
-		System.out.println("Caps change");
 	}
 	
 	public void display(){
